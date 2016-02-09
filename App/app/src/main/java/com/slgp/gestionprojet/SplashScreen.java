d7e68ed393d84e82d18d441066d9c65550ef699e
godefroy29 <godefroy.29@gmail.com>
2016-02-09 14:41:39 +0100

    ajout fonction init dans splashscreen

--- App/app/src/main/java/com/slgp/gestionprojet/SplashScreen.java
+++ App/app/src/main/java/com/slgp/gestionprojet/SplashScreen.java
@@ -1,13 +1,40 @@
 package com.slgp.gestionprojet;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
+
+    private ProgressBar progressBar;
+    private Button btnlocal;
+    private Button btnConnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        initialize();
+
+        setListener();
+    }
+
+    private void setListener() {
+
+    }
+
+    private void initialize() {
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnlocal = (Button) findViewById(R.id.btnLocal);
+        btnConnexion = (Button) findViewById(R.id.btnConnexion);
+
     }
 }
